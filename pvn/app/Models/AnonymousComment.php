4a9033e942cda4f499e9280514b9e908daf28382
Hanane Oubaha <Hananeouba03@gmail.com>
2025-05-02 01:31:35 +0100

    jai completer les models pour les posts anonyms avec les fillables et tout

--- pvn/app/Models/AnonymousComment.php
+++ pvn/app/Models/AnonymousComment.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AnonymousComment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'anonymous_post_id',
+        'user_id',
+        'is_psychologist'
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(AnonymousPost::class, 'anonymous_post_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- pvn/app/Models/AnonymousPost.php
+++ pvn/app/Models/AnonymousPost.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AnonymousPost extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'user_id',
+        'anonymous_id',
+        'notify_email',
+        'support_count'
+    ];
+
+    protected $attributes = [
+        'support_count' => 0,
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(AnonymousComment::class);
+    }
 }
